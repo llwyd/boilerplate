62384c42356136f6af7f4376a8dcb0e756caa990
llwyd <tom@llwyd.io>
2025-06-15 21:39:24 +0100

    improved event heap

--- src/event_heap.c
+++ src/event_heap.c
@@ -1,11 +1,12 @@
 #include "event_heap.h"
 
-static void swap( heap_data_t * a, heap_data_t * b )
+static void swap( heap_data_t ** a, heap_data_t ** b )
 {
     ASSERT(a != NULL);
     ASSERT(b != NULL);
 
-    heap_data_t temp = *a;
+    
+    heap_data_t * temp = *a;
     *a = *b;
     *b = temp;
 }
@@ -14,11 +15,17 @@ extern void Heap_Init(heap_t * heap)
 {
     heap->fill = 0;
     heap->max = HEAP_LEN;
-    uint8_t * heap8 = (uint8_t *)&heap->heap;
+    
+    uint8_t * heap8 = (uint8_t *)&heap->buffer;
     uint32_t size = sizeof(heap_data_t) * HEAP_LEN;
     for(uint32_t idx = 0; idx < size; idx++)
     {
         *(heap8++) = 0xFF; 
+    }
+
+    for(uint32_t idx = 0; idx < HEAP_LEN; idx++)
+    {
+        heap->heap[idx] = &heap->buffer[idx];
     }
 }
 
@@ -43,7 +50,7 @@ extern void Heap_Push(heap_t * heap, heap_data_t data)
     ASSERT(heap->fill < heap->max);
 
     /* Add the value to the bottom of the heap */
-    heap->heap[heap->fill] = data;
+    *heap->heap[heap->fill] = data;
 
     uint32_t idx = heap->fill;
 
@@ -51,7 +58,7 @@ extern void Heap_Push(heap_t * heap, heap_data_t data)
     while( idx > 0U )
     {
         uint32_t parent = (idx - 1) >> 1U;
-        if( heap->heap[idx].key < heap->heap[parent].key )
+        if( heap->heap[idx]->key < heap->heap[parent]->key )
         {
             swap(&heap->heap[idx], &heap->heap[parent]);
         }
@@ -66,7 +73,7 @@ extern uint32_t Heap_Peek(heap_t * heap)
     ASSERT(!Heap_IsEmpty(heap));
     ASSERT(heap->fill > 0U);
     
-    return heap->heap[0U].key;
+    return heap->heap[0U]->key;
 }
 
 extern uint32_t Heap_Pop(heap_t * heap)
@@ -75,11 +82,11 @@ extern uint32_t Heap_Pop(heap_t * heap)
     ASSERT(!Heap_IsEmpty(heap));
     ASSERT(heap->fill > 0U);
 
-    uint32_t top = heap->heap[0].key;
+    uint32_t top = heap->heap[0]->key;
 
     /* Place bottom of heap at top */
-    heap->heap[0] = heap->heap[heap->fill - 1U];
-    heap->heap[heap->fill - 1U].key = UINT32_MAX;
+    swap(&heap->heap[0], &heap->heap[heap->fill - 1U]);
+    heap->heap[heap->fill - 1U]->key = UINT32_MAX;
     heap->fill--;
 
     /* Sink through the heap */
@@ -88,12 +95,12 @@ extern uint32_t Heap_Pop(heap_t * heap)
 
     while(jdx < heap->fill)
     {
-        if(heap->heap[jdx].key > heap->heap[jdx+1].key)
+        if(heap->heap[jdx]->key > heap->heap[jdx+1]->key)
         {
             jdx++;
         }
 
-        if(heap->heap[idx].key > heap->heap[jdx].key)
+        if(heap->heap[idx]->key > heap->heap[jdx]->key)
         {
             swap(&heap->heap[idx], &heap->heap[jdx]);
             idx = jdx;
@@ -114,11 +121,11 @@ extern heap_data_t Heap_PopFull(heap_t * heap)
     ASSERT(!Heap_IsEmpty(heap));
     ASSERT(heap->fill > 0U);
 
-    heap_data_t top = heap->heap[0];
+    heap_data_t top = *heap->heap[0];
 
     /* Place bottom of heap at top */
-    heap->heap[0] = heap->heap[heap->fill - 1U];
-    heap->heap[heap->fill - 1U].key = UINT32_MAX;
+    swap(&heap->heap[0], &heap->heap[heap->fill - 1U]);
+    heap->heap[heap->fill - 1U]->key = UINT32_MAX;
     heap->fill--;
 
     /* Sink through the heap */
@@ -127,12 +134,12 @@ extern heap_data_t Heap_PopFull(heap_t * heap)
 
     while(jdx < heap->fill)
     {
-        if(heap->heap[jdx].key > heap->heap[jdx+1].key)
+        if(heap->heap[jdx]->key > heap->heap[jdx+1]->key)
         {
             jdx++;
         }
 
-        if(heap->heap[idx].key > heap->heap[jdx].key)
+        if(heap->heap[idx]->key > heap->heap[jdx]->key)
         {
             swap(&heap->heap[idx], &heap->heap[jdx]);
             idx = jdx;
